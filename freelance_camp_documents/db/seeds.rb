cd9805d5ddaad4812265680e97d8dfe3ffd563f4
brianleeanderson19 <brianleeanderson19@yahoo.com>
2017-03-07 12:37:37 -0700

    updated seeds file

---
+++ freelance_camp_documents/db/seeds.rb
@@ -0,0 +1,8 @@
+10.times do |d|
+  FreelanceDocument.create!(
+    title: "Document #{d}", 
+    description: "Here is the Description",
+    file_url: "https://example.com",
+    image_url: "https://static.pexels.com/photos/235310/pexels-photo-235310.jpeg" 
+    )
+end
