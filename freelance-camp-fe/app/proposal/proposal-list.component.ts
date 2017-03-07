b622ea40515fd325ab6d872ee673c36cf6010f90
brianleeanderson19 <brianleeanderson19@yahoo.com>
2017-03-07 16:10:33 -0700

    Updated proposal show component with api data

--- freelance-camp-fe/app/proposal/proposal-list.component.ts
+++ freelance-camp-fe/app/proposal/proposal-list.component.ts
@@ -1,20 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 import { Proposal } from './proposal';
+import { ProposalService } from './proposal.service';
 
 @Component({
   moduleId: module.id,
   selector: 'proposal-list',
   templateUrl: 'proposal-list.component.html',
-  styleUrls: ['proposal-list.component.css']
+  styleUrls: ['proposal-list.component.css'],
+  providers: [ ProposalService ]
 })
-export class ProposalListComponent {
-  proposalOne: Proposal = new Proposal(20, 'ABCcompany', 'http://brianandersondev.com', 'Ruby on Rails', 150, 80, 15, 'brianleeanderson19@yahoo.com')
-  proposalTwo: Proposal = new Proposal(50, 'XYZ company', 'http://brianandersondev.com', 'Ruby on Rails', 150, 80, 15, 'brianleeanderson19@yahoo.com')
-  proposalThree: Proposal = new Proposal(330, 'The company', 'http://brianandersondev.com', 'Ruby on Rails', 150, 80, 15, 'brianleeanderson19@yahoo.com')
+export class ProposalListComponent implements OnInit {
+  proposals: Proposal[];
+  errorMessage: string; 
+  mode = "Observable";
 
-  proposals: Proposal[] = [
-    this.proposalOne, 
-    this.proposalTwo, 
-    this.proposalThree
-  ]
+  constructor(
+    private proposalService: ProposalService
+    ) {}
+
+  ngOnInit() {
+    let timer = Observable.timer(0, 5000);
+    timer.subscribe(() => this.getProposals());
+  }
+
+  getProposals() {
+    this.proposalService.getProposals()
+        .subscribe(
+          proposals => this.proposals = proposals,
+          error => this.errorMessage = <any>error
+        );
+  }
 }
